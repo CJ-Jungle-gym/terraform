0a47a624b53fc01d9fa699e19f1a26ff547097ea
jemsgusting <tyler010333@gmail.com>
2025-02-25 19:12:08 +0900

    Update ec2.test.tf

--- hyuna/ec2.test.tf
+++ hyuna/ec2.test.tf
@@ -2,12 +2,51 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_instance" "test_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0"  
-  instance_type = "t2.micro"
+# ✅ 기존 VPC 참조
+data "aws_vpc" "existing_vpc" {
+  id = "vpc-061260f6e81150f73"
+}
 
-  tags = {
-    Name = "Terraform-Test-EC2"
+# ✅ 해당 VPC의 기본 서브넷 가져오기
+data "aws_subnet" "default_subnet" {
+  vpc_id = data.aws_vpc.existing_vpc.id
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
   }
 }
 
+# ✅ 보안 그룹 생성 (SSH 허용)
+resource "aws_security_group" "test_sg" {
+  vpc_id = data.aws_vpc.existing_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Test-SG"
+  }
+}
+
+# ✅ EC2 인스턴스 생성 (이름: hyuna_lambda_test)
+resource "aws_instance" "hyuna_lambda_test" {
+  ami             = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
+  instance_type   = "t2.micro"
+  subnet_id       = data.aws_subnet.default_subnet.id
+  security_groups = [aws_security_group.test_sg.name]
+
+  tags = {
+    Name = "hyuna_lambda_test"
+  }
+}
